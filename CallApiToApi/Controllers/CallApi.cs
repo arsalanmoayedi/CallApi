30162cab3ec413149b9dd8fe466e5d9e47d149bb
arsalanmoayedi <moayediamirarsalan@gmail.com>
2023-08-25 14:47:14 +0330

    first commit to send to github

---
+++ CallApiToApi/Controllers/CallApi.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+using Microsoft.AspNetCore.Authorization;
+
+namespace CallApiToApi.Controllers
+{
+    [Route("api/callApi")]
+    [ApiController]
+    public class CallApiController : ControllerBase
+    {
+        [AllowAnonymous]
+        [HttpGet("result")]
+        public async Task<ActionResult> callapitoapi()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://api.zarbit.ir/api/Publicapi/");
+                using (HttpResponseMessage response = await client.GetAsync("GetPairs"))
+                {
+                    var responsecontect = response.Content.ReadAsStringAsync().Result;
+                    response.EnsureSuccessStatusCode();
+                    return Ok(responsecontect);
+                }
+            }
+        }
+
+    }
+}
